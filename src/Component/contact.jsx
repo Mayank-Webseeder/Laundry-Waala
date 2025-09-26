574aa1a3631e6b70aaf0ec63a69fc7e66c133eff
sonuweb089 <sonu@webseeder.tech>
2025-09-26 15:58:33 +0530

    change no

--- src/Component/contact.jsx
+++ src/Component/contact.jsx
@@ -11,7 +11,7 @@ const contact = () => {
           <Phone size={24} />
         </a>
         <a
-          href="https://wa.me/919876543210"
+          href="https://wa.me/919876543610"
           target="_blank"
           rel="noreferrer"
           className="bg-green-500 p-3 rounded-full shadow text-white"
